91aeb87caa775f83abfc82dc295376cc1b3ad43b
yhordan <yhordanaguilar69@gmail.com>
2023-07-25 16:31:38 -0500

    gateway

--- eureka-service/src/main/java/com/eureka/service/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/eureka/service/EurekaServiceApplication.java
@@ -2,10 +2,11 @@ package com.eureka.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
