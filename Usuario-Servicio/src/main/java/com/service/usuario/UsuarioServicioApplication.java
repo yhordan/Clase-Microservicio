74112ab527565d7639a8b648e436ee6f4f9ecbe2
yhordan <yhordanaguilar69@gmail.com>
2023-07-25 11:58:21 -0500

    eureka

--- Usuario-Servicio/src/main/java/com/service/usuario/UsuarioServicioApplication.java
+++ Usuario-Servicio/src/main/java/com/service/usuario/UsuarioServicioApplication.java
@@ -3,12 +3,14 @@ package com.service.usuario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
 @EnableFeignClients
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
+@EnableDiscoveryClient
 public class UsuarioServicioApplication {
 
 	public static void main(String[] args) {
