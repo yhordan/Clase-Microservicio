62ab644a8744ecc5407ce3348f7f1a480537c7b6
yhordan <yhordanaguilar69@gmail.com>
2023-07-25 11:54:56 -0500

    eureka

--- Moto-Servicio/src/main/java/com/service/moto/MotoServicioApplication.java
+++ Moto-Servicio/src/main/java/com/service/moto/MotoServicioApplication.java
@@ -2,8 +2,10 @@ package com.service.moto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MotoServicioApplication {
 
 	public static void main(String[] args) {
